7a1397809db498a867527bad4d01b9311f07ac2d
Boris Zekic <bzekic@spartaglobal.com>
2018-11-12 12:03:02 +0000

    initial commit with a created selenium example project

---
+++ src/main/java/com/sparta/bz/App.java
@@ -0,0 +1,13 @@
+package com.sparta.bz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
